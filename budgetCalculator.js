33f3d73d9ef546e4ccc24414ec43f3841219299c
Hartlna1 <nan.hartley@gmail.com>
2013-10-10 14:17:22 -0400

    configured all processing for output.

--- budgetCalculator.js
+++ budgetCalculator.js
@@ -6,7 +6,7 @@ Date: 			10/9/2013
 Description: 	Allows the user to input expenses for budget categories, and returns percentage of budget spent on each.
 */
 
-/*//declaration and instantiation of variables and constants
+//declaration and instantiation of variables and constants
 var BR = "<br />";						//HTML line break
 var ES = " ";							//HTML extra space
 var LE = ".";							//HTML line end
@@ -17,24 +17,40 @@ var other = 0.0; 						//other expenses variable
 var total = 0.0;						//total expenses
 var percentage_rent = 0.0;				//percentage rent of total budget
 var percentage_food = 0.0;				//percentage food of total budget
-var percentage_utilites = 0.0;			//percentage utilities of total budget
-var percentage_other = 0.0;				//percentage of total budget for all other expenses		*/
+var percentage_utilities = 0.0;			//percentage utilities of total budget
+var percentage_other = 0.0;				//percentage of total budget for all other expenses		
 
 //Welcome the user to the program
-document.write("Welcome to the budget percentage calculator!  We will analyze your budget based on your rent, food, utilities, and all other expenses.");
+document.write("Welcome to the budget percentage calculator!  We will analyze your budget based on your rent, food, utilities, and all other expenses." + BR + BR);
 
 //Getting user input
 rent = prompt("What is your monthly rent payment?");
-parseFloat(rent);
+rent = parseFloat(rent);
 food = prompt("What are your monthly expenses on food?");
-parseFloat(food);
+food = parseFloat(food);
 utilities = prompt("What are your monthly expenses on utilities?");
-parseFloat(utilities);
+utilities = parseFloat(utilities);
 other = prompt("What are the montly expenses on all other items?");
-parseFloat(other);
+other = parseFloat(other);
 
 //Processing input to provide output 
+total = rent + food + utilities + other;					//total of all budget items
+percentage_rent = (rent * 100)/total;						//% of total that is rent
+percentage_food = (food * 100)/total;						//% of total that is food
+percentage_utilities = (utilities * 100)/total;				//% of total that is utilities
+percentage_other = (other * 100)/total;						//% of total that is all other expenses
 
 //Output of data
+//document.write("Your rent: $" + rent.toFixed(2) + BR);
+//document.write("Your food: $" + food.toFixed(2) + BR);
+//document.write("Your utilities: $" + utiliites.toFixed(2) + BR);
+//document.write("Your other expenses: $" + other.toFixed(2) + BR);
+//document.write("The total of your monthly budget is: $" + total.toFixed(2) + LE + BR);
+//document.write("The rent percentage is:" + ES + rent_percentage.toFixed(1) + "%" + LE + BR);
+//document.write("The food percentage is:" + ES + food_percentage.toFixed(1) + "%" + LE + BR);
+//document.write("The utilities percentage is:" + ES + utilities_percentage.toFixed(1) + "%" + LE + BR);
+//document.write("The percentage for all other expenses is: " + ES + other_percentage.toFixed(1) + "%" + LE + BR);
+
 
 //Thank the user and end the program
+//document.write("Thank you for using our Budget Percentage Calculator!  Have a nice day!")
