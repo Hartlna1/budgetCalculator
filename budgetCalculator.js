067764d595ef59304de3a4b34775611bb9a6b9a5
Hartlna1 <nan.hartley@gmail.com>
2013-10-10 12:57:20 -0400

    Initial commit of empty HTML and JS files for the Budget Calculator.

---
+++ budgetCalculator.js
@@ -0,0 +1,22 @@
+/* 
+Programmer: 	Nanette K. Hartley
+Language: 		JavaScript
+Filename:  		budgetCalculator.js
+Date: 			10/9/2013
+Description: 	Allows the user to input expenses for budget categories, and returns percentage of budget spent on each.
+*/
+
+//declaration of variables and constants
+var BR = "<br />";						//HTML line break
+var ES = " ";							//HTML extra space
+var LE = ".";							//HTML line end
+
+//Welcome the user to the program
+
+//Getting user input
+
+//Processing input to provide output 
+
+//Output of data
+
+//Thank the user and end the program
