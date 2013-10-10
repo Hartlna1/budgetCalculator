9bb36120be56c3fb7ce7a211575c1eb8ca1cdcaa
Hartlna1 <nan.hartley@gmail.com>
2013-10-10 13:21:45 -0400

    declaration and instatiation of variables

--- budgetCalculator.js
+++ budgetCalculator.js
@@ -6,10 +6,19 @@ Date: 			10/9/2013
 Description: 	Allows the user to input expenses for budget categories, and returns percentage of budget spent on each.
 */
 
-//declaration of variables and constants
+//declaration and instantiation of variables and constants
 var BR = "<br />";						//HTML line break
 var ES = " ";							//HTML extra space
 var LE = ".";							//HTML line end
+var rent = 0.0;							//rent variable
+var food = 0.0;							//food variable
+var utilities = 0.0;					//utilites variable
+var other = 0.0; 						//other expenses variable
+var total = 0.0;						//total expenses
+var percentage_rent = 0.0;				//percentage rent of total budget
+var percentage_food = 0.0;				//percentage food of total budget
+var percentage_utilites = 0.0;			//percentage utilities of total budget
+var percentage_other = 0.0;				//percentage of total budget for all other expenses		
 
 //Welcome the user to the program
 
